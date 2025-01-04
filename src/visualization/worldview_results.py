26ba9d32e8aaf95ffabb3e1d580c32c2dd9f98d3
Bry Willis <microglyphics@gmail.com>
2025-01-04 12:03:34 -0500

    func: fox plot chart on PDF

--- src/visualization/worldview_results.py
+++ src/visualization/worldview_results.py
@@ -11,7 +11,7 @@ import time
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 @st.cache_data
