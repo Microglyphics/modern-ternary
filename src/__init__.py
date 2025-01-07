f31e76cbac2059a4ceac878060f63eba0042e488
Bry Willis <microglyphics@gmail.com>
2025-01-07 01:54:17 -0500

    update data viewer for new schema

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,6 @@
 Modern Ternary Survey Application
 """
 
-__version__ = '1.1.0'
+__version__ = '1.1.4'
 __author__ = 'Microglyphics'
 __description__ = 'Interactive ternary chart questionnaire for exploring conceptual frameworks'
