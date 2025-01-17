ab7a4222ab59521e01b4728ca208df7dfcbc3211
Bry Willis <microglyphics@gmail.com>
2025-01-17 13:36:18 -0500

    server consolidATION

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from src.ui.streamlit_app import main
-
-if __name__ == "__main__":
-    main()
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,19 +2,9 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {}
-	},
-
-	plugins: [typography]
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {}
+    },
+    plugins: [typography]
 };
-
-module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-	  extend: {},
-	},
-	plugins: [],
-  }
